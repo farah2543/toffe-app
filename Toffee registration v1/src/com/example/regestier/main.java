be25aadc0e11e2dfe63d6f88f7a8a16387cbe0bd
mariammmg <95446903+mariammmg@users.noreply.github.com>
2023-05-06 16:45:21 +0300

    Add files via upload
    This is my work,it need some changes but i sent it to you to see what was done

---
+++ Toffee registration v1/src/com/example/regestier/main.java
@@ -0,0 +1,15 @@
+package com.example.regestier;
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) throws IOException
+    {
+        //TODO-implement
+        System.out.println("hello world");
+    }
+}
+
