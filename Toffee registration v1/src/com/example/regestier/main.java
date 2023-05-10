b4e42ba24f9d2b74e4fad28031ae477c1f2b8815
mariammmg <95446903+mariammmg@users.noreply.github.com>
2023-05-10 15:54:14 +0300

    Add files via upload
    need some modifications on regex

---
+++ Toffee registration v1/src/com/example/regestier/main.java
@@ -0,0 +1,20 @@
+package com.example.regestier;
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) throws IOException
+    {
+
+        //Buyer x=new Buyer();
+      // x.create_order();
+      //  x.register();
+        System.out.println("hello world");
+    }
+
+
+}
+
